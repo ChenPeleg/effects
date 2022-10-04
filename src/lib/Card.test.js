7429d67407caefb2de1eb422d6c1cd6f7a67e4ff
chenpeleg <chenpeleg@gmail.com>
2022-10-04 16:31:54 +1100

    added babble config for jest testing

---
+++ src/lib/Card.test.js
@@ -0,0 +1,8 @@
+import { render, fireEvent, screen } from '@testing-library/svelte';
+//import * as Card from './Card.svelte';
+it('about page has expected h1', () => {
+	//const { getByText, getByTestId } = render(Card);
+	// const cardBody = getByTestId('single-card');
+	// expect(cardBody).toBeTruthy();
+	expect(8).toBe(8);
+});
