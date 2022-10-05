c231812c3f48c0aba81b7e71f27fafa773e31492
chenpeleg <chenpeleg@gmail.com>
2022-10-05 16:24:38 +1100

    added font

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,24 +1,24 @@
 <script>
 	import Layout from '../lib/Layout.svelte';
 	import Header from './Header.svelte';
+	import '../assets/css/fonts.css';
 </script>
+
 <svelte:head>
-	<meta title="Effects"/>
+	<meta title="Effects" />
 	<title>Effects</title>
 </svelte:head>
-<div  class="app">
-	 <Header></Header>
+<div class="app">
+	<Header />
 
-	<main>
-		
-
-	</main>
+	<main />
 	<slot />
 	<Layout />
-	<footer>
-	</footer>
-
+	<footer />
 </div>
 
-
-
+<style>
+	:root {
+		font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+	}
+</style>
