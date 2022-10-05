9c29311bd22f7adceae25bc06319e08d5dc1d866
chenpeleg <chenpeleg@gmail.com>
2022-10-05 14:00:05 +1100

    added id to cards for the testing

--- src/lib/Card.test.js
+++ src/lib/Card.test.js
@@ -1,9 +1,9 @@
 import { render, fireEvent, screen } from '@testing-library/svelte';
 import Card from './Card.svelte';
 
-const cardContent = { color: 'green', content: 'car keys', name: 'car' };
+const cardContent = { color: 'green', content: 'car keys', name: 'car', id: 1 };
 it('about page has expected h1', () => {
 	const { getByText, getByTestId } = render(Card, { cardContent: cardContent, isCurrent: false });
-	const cardBody = getByTestId('single-card');
+	const cardBody = getByTestId('single-card-1');
 	expect(cardBody).toBeTruthy();
 });
