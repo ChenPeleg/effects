baaf711979f6ac9d1342413147131cb2269e4618
chenpeleg <chenpeleg@gmail.com>
2022-10-07 18:46:13 +1100

    added try dist folder

--- src/lib/Card.svelte
+++ src/lib/Card.svelte
@@ -1,49 +1,49 @@
 <script>
-    import { MainStore } from "../store/store.main";
+  import { MainStore } from "../store/store.main";
 
-    import { createEventDispatcher } from "svelte";
+  import { createEventDispatcher } from "svelte";
 
-    /**@type {CardContent} */
-    export let cardContent;
-    /**@type {boolean}*/
-    export let isCurrent;
-    let clicks = 0;
+  /**@type {CardContent} */
+  export let cardContent;
+  /**@type {boolean}*/
+  export let isCurrent;
+  let clicks = 0;
 
-    /**@type {MouseEventHandler} */
-    const clickCard = (ev) => {
-        MainStore.update((s) => {
-            s.currentCard = cardContent.id || 0;
-            return s;
-        });
-        clicks++;
-    };
+  /**@type {MouseEventHandler} */
+  const clickCard = (ev) => {
+    MainStore.update((s) => {
+      s.currentCard = cardContent.id || 0;
+      return s;
+    });
+    clicks++;
+  };
 </script>
 
 <div on:click={clickCard} class="card-wrapper">
-    <div class="card-body">
-        {cardContent.name}
-        {isCurrent ? "Its Current" : ""}
-    </div>
+  <div class="card-body">
+    {cardContent.name}
+    {isCurrent ? "Its Current" : ""}
+  </div>
 </div>
 
 <style>
-    div.card-wrapper {
-        display: inline-flex;
-    }
-    div.card-body {
-        display: inline-flex;
-        margin: 0px 0px;
-        width: 15vw;
-        min-width: 100px;
-        padding: 1rem;
-        height: 10vh;
-        background-color: white;
-        border-radius: 1rem;
-        box-shadow: 2px 2px 10px 2px black;
-        cursor: pointer;
-        transition: box-shadow 200ms ease-in-out;
-    }
-    div.card-body:hover {
-        box-shadow: 2px 2px 15px 2px black;
-    }
+  div.card-wrapper {
+    display: inline-flex;
+  }
+  div.card-body {
+    display: inline-flex;
+    margin: 0px 0px;
+    width: 15vw;
+    min-width: 100px;
+    padding: 1rem;
+    height: 10vh;
+    background-color: white;
+    border-radius: 1rem;
+    box-shadow: 2px 2px 10px 2px black;
+    cursor: pointer;
+    transition: box-shadow 200ms ease-in-out;
+  }
+  div.card-body:hover {
+    box-shadow: 2px 2px 15px 2px black;
+  }
 </style>
