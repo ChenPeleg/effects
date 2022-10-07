49e15fd4d82bb95b989356ab62391a7d94d842de
chenpeleg <chenpeleg@gmail.com>
2022-10-07 22:53:22 +1100

    configured assets to bbe th public dir

--- src/store/store.main.js
+++ src/store/store.main.js
@@ -1,13 +1,13 @@
-import { writable } from 'svelte/store';
-import { CardsStore } from './store.cards';
+import { writable } from "svelte/store";
+import { CardsStore } from "./store.cards";
 
 const MAXIMUM_SOUND_TIME = 2000;
 
 /**@type{MainStore} */
 const initialStore = {
-	user: '',
-	currentCard: 0,
-	currentCardContent: null
+  user: "",
+  currentCard: 0,
+  currentCardContent: null,
 };
 
 /**
@@ -20,39 +20,39 @@ let audio;
 CardsStore.subscribe((c) => (allCards = c));
 
 const CreateMainStore = () => {
-	const { subscribe, set, update } = writable(initialStore);
-	/**@type {NodeJS.Timeout } */
-	let timeoutRef;
-	const cardChosen = (/** @type {number} */ cardId) => {
-		update((s) => {
-			s.currentCard = cardId || 0;
-			const card = allCards.find((c) => c.id === cardId);
-			const fileName = card?.content || 'car_remote';
-			if (!audio) {
-				audio = new Audio();
-				audio.addEventListener('ended', (event) => {
-					update((s) => {
-						s.currentCard = 0;
-						return s;
-					});
-				});
-			}
-			audio.setAttribute('src', `audio/${fileName}.mp3`);
-			audio.load();
-			audio.play();
-			return s;
-		});
-		clearTimeout(timeoutRef);
-		timeoutRef = setTimeout(() => {
-			// update((s) => {
-			// 	s.currentCard = 0;
-			// 	return s;
-			// });
-		}, MAXIMUM_SOUND_TIME);
-	};
-	return {
-		subscribe,
-		cardChosen
-	};
+  const { subscribe, set, update } = writable(initialStore);
+  /**@type {NodeJS.Timeout } */
+  let timeoutRef;
+  const cardChosen = (/** @type {number} */ cardId) => {
+    update((s) => {
+      s.currentCard = cardId || 0;
+      const card = allCards.find((c) => c.id === cardId);
+      const fileName = card?.content || "car_remote";
+      if (!audio) {
+        audio = new Audio();
+        audio.addEventListener("ended", (event) => {
+          update((s) => {
+            s.currentCard = 0;
+            return s;
+          });
+        });
+      }
+      audio.setAttribute("src", `/audio/${fileName}.mp3`);
+      audio.load();
+      audio.play();
+      return s;
+    });
+    clearTimeout(timeoutRef);
+    timeoutRef = setTimeout(() => {
+      // update((s) => {
+      // 	s.currentCard = 0;
+      // 	return s;
+      // });
+    }, MAXIMUM_SOUND_TIME);
+  };
+  return {
+    subscribe,
+    cardChosen,
+  };
 };
 export const MainStore = CreateMainStore();
