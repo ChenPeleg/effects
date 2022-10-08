d840cd61b15f8c7a76a7b91c00d20c80d6d8bc79
chenpeleg <chenpeleg@gmail.com>
2022-10-08 18:42:42 +1100

    added font

--- src/lib/Layout.svelte
+++ src/lib/Layout.svelte
@@ -14,6 +14,30 @@
   div.app-wrapper {
     width: 100vw;
   }
+  @font-face {
+    font-family: "Balsamiq Sans";
+    font-style: normal;
+    font-weight: 500;
+    src: url("/fonts/balsamiq-sans/BalsamiqSans-Regular.ttf") format("truetype");
+  }
+  @font-face {
+    font-family: "Balsamiq Sans";
+    font-style: bold;
+    font-weight: 700;
+    src: url("/fonts/balsamiq-sans/BalsamiqSans-Bold.ttf") format("truetype");
+  }
+  @font-face {
+    font-family: "Signika Negative";
+    font-style: normal;
+    font-weight: 500;
+    src: url("/fonts/SignikaNegative-Medium.ttf") format("truetype");
+  }
+  @font-face {
+    font-family: "PatrickHand-Regular";
+    font-style: normal;
+    font-weight: 500;
+    src: url("/fonts/PatrickHand-Regular.ttf") format("truetype");
+  }
   :global(:root) {
     font-family: "Gill Sans", "Gill Sans MT", Calibri, "Trebuchet MS",
       sans-serif;
@@ -27,7 +51,8 @@
       rgba(234, 236, 160, 1) 3%,
       rgba(250, 255, 105, 1) 81%
     );
-    /* font-family: "Signika_Negative"; */
+    font-family: "Signika Negative", sans-serif;
+    font-family: "Balsamiq Sans", sans-serif;
     --main-box-shadow: 0 1px 2px 0 rgb(60 64 67 / 30%),
       0 1px 3px 1px rgb(60 64 67 / 15%);
     --main-box-shadow-hover: 0 1px 3px 0 rgb(60 64 67 / 30%),
