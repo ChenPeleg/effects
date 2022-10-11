0773a9ea969cf4e5bcdd360058549debb3811978
chenpeleg <chenpeleg@gmail.com>
2022-10-11 21:53:30 +1100

    added routes

---
+++ src/lib/router/route.svelte
@@ -0,0 +1,16 @@
+<script>
+  import { pathname } from "src/store/store.path";
+  import { onDestroy } from "svelte";
+
+  export let route;
+  let path;
+
+  const unsubscribe = pathname.subscribe((p) => (path = p));
+  onDestroy(unsubscribe);
+</script>
+
+<div id={"route-" + route}>
+  <div>
+    {$$props}
+  </div>
+</div>
