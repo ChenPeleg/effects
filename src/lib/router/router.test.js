bed25684f9c192f7d4b2d8fd71674460c761f6e3
chenpeleg <chenpeleg@gmail.com>
2022-10-14 21:54:57 +0300

    testing the router component

--- src/lib/router/router.test.js
+++ src/lib/router/router.test.js
@@ -4,7 +4,7 @@ import { pathStore } from "../../store/store.path";
 import Route from "./route.svelte";
 
 describe("router", () => {
-  it("show route correctly", () => {
+  it("renders routes if the rout fits the path", () => {
     const route = "abc";
     const chosenSpy = jest
       .spyOn(pathStore, "subscribe")
@@ -13,7 +13,18 @@ describe("router", () => {
     const slot = getByTestId("route-" + route + "-active");
     expect(slot).toBeTruthy();
   });
-  it("show route correctly with function description", () => {
+  it("does not renders routes if the route doesn't fits the path", () => {
+    const route = "abc123";
+    const chosenSpy = jest
+      .spyOn(pathStore, "subscribe")
+      .mockImplementation(readable(route).subscribe);
+    const res = render(Route, { route: "abc1234" });
+    const slot = res.container.querySelector(
+      `[data-testid="route-${route}-active"]`
+    );
+    expect(slot).toBeFalsy();
+  });
+  it("renders routes if the rout fits the path with function description", () => {
     const route = "efg";
     const chosenSpy = jest
       .spyOn(pathStore, "subscribe")
